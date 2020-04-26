a6eb7968a2ccd04c811999992454e91d4b4f381a
shanedroogan <shanedroogan@gmail.com>
2020-04-26 17:00:51 -0400

    removed dataframe copy in create_crawl

--- app/utils.py
+++ app/utils.py
@@ -43,7 +43,6 @@ def create_crawl(user_lat = 40.734198,user_long=-73.988325, date=datetime.date.t
     #initialize empty list, which will be what we return. It should contain the ID
     return_list = []
     
-    df2 = df.copy()
     #save user_lat,user_long to temp values, which will be changed since distance is calculated relative to last location
     temp_long = user_long
     temp_lat = user_lat
@@ -52,22 +51,22 @@ def create_crawl(user_lat = 40.734198,user_long=-73.988325, date=datetime.date.t
     while len(return_list) < 5:
         
         #calculate the distance and score
-        df2['distance'] = df2.apply(lambda x: distance(temp_lat, temp_long, x['latitude'], x['longitude']), axis = 1)
+        df['distance'] = df.apply(lambda x: distance(temp_lat, temp_long, x['latitude'], x['longitude']), axis = 1)
         
         #score function
-        df2['score'] = ((4-df2['price'])/4 + df2['rating']/5 - df2['distance'])/3
+        df['score'] = ((4-df['price'])/4 + df['rating']/5 - df['distance'])/3
 
         #sort values, take top value 
-        df2.sort_values(by = ['score'],ascending = False, inplace = True)
+        df.sort_values(by = ['score'],ascending = False, inplace = True)
         
         #append top value to return_list
-        return_list.append(int(df2[:1].index.values))
+        return_list.append(int(df[:1].index.values))
  
         #update temp_long,temp_lat
-        temp_long = df2.loc[int(df2[:1].index.values),'longitude']
-        temp_lat = df2.loc[int(df2[:1].index.values), 'latitude'] 
+        temp_long = df.loc[int(df[:1].index.values),'longitude']
+        temp_lat = df.loc[int(df[:1].index.values), 'latitude'] 
         
         #drop bar from df
-        df2.drop([int(df2[:1].index.values)],inplace = True) 
+        df.drop([int(df[:1].index.values)],inplace = True) 
     return return_list
     
